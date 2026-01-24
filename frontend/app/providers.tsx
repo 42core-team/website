3d97814f1299ca96b7e628a2f21f3ee647040b9f
Emil Ebert <emilebert@icloud.com>
2026-01-24 14:15:17 +0100

    install react-query

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +14,7 @@ export default function Providers({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
