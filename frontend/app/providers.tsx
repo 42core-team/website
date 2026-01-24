ddd6aed3e71d4bf9d60c59a7d89d81f3c91715fe
Emil Ebert <emilebert@icloud.com>
2026-01-24 13:27:54 +0100

    remove dev tools because build error

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +12,6 @@ export default function Providers({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
