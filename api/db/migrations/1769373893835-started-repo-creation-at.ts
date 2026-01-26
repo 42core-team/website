4b4bb6b39a21c35ea54d92efbc6949e316cae024
Emil Ebert <emilebert@icloud.com>
2026-01-26 16:11:40 +0100

    update migration to set default value for startedRepoCreationAt in teams table

--- api/db/migrations/1769373893835-started-repo-creation-at.ts
+++ api/db/migrations/1769373893835-started-repo-creation-at.ts
@@ -4,7 +4,13 @@ export class StartedRepoCreationAt1769373893835 implements MigrationInterface {
     name = 'StartedRepoCreationAt1769373893835'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "teams" ADD "startedRepoCreationAt" TIMESTAMP`);
+      await queryRunner.query(
+        `ALTER TABLE "teams" ADD "startedRepoCreationAt" TIMESTAMP`,
+      );
+
+      await queryRunner.query(
+        `UPDATE "teams" SET "startedRepoCreationAt" = NOW() WHERE "startedRepoCreationAt" IS NULL`,
+      );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
