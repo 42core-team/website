cfa4f75e323abdc53221a1d60ac752528d22e8b8
Emil Ebert <emilebert@icloud.com>
2026-07-25 13:45:25 +0200

    rename template

--- github-service/src/repo.utils.ts
+++ github-service/src/repo.utils.ts
@@ -359,7 +359,7 @@ export class RepoUtils {
       const originalContent = await fs.readFile(mainCPath, "utf-8");
 
       const updatedContent = originalContent.replaceAll(
-        "my CORE Bot",
+        "My CORE Bot",
         teamName,
       );
 
