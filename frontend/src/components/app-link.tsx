64d0aafbfd0faba47c6d50fa9a5c69c73770429d
Paul Großmann <git@paulgrossmann.de>
2026-08-01 23:04:42 +0200

    Revert "fix home navigation active state"
    This reverts commit 8fb277bce112b17a15132af4c142cb81c0835b77.

--- frontend/src/components/app-link.tsx
+++ frontend/src/components/app-link.tsx
@@ -1,35 +1,20 @@
-import { Link as RouterLink } from '@tanstack/react-router'
-import type { ActiveOptions } from '@tanstack/react-router'
-import React from 'react'
+import { Link as RouterLink } from "@tanstack/react-router";
+import React from "react";
 
 type AppLinkProps = Omit<
   React.AnchorHTMLAttributes<HTMLAnchorElement>,
-  'href'
+  "href"
 > & {
-  activeOptions?: ActiveOptions
-  href?: string
-  to?: string
-}
+  href?: string;
+  to?: string;
+};
 
-export default function Link({
-  activeOptions,
-  href,
-  to,
-  ...props
-}: AppLinkProps) {
-  const target = to || href || '/'
+export default function Link({ href, to, ...props }: AppLinkProps) {
+  const target = to || href || "/";
 
-  if (/^(https?:|mailto:|tel:)/.test(target) || props.target === '_blank') {
-    return <a href={target} {...props} />
+  if (/^(https?:|mailto:|tel:)/.test(target) || props.target === "_blank") {
+    return <a href={target} {...props} />;
   }
 
-  return (
-    <RouterLink
-      to={target}
-      activeOptions={
-        activeOptions ?? (target === '/' ? { exact: true } : undefined)
-      }
-      {...props}
-    />
-  )
+  return <RouterLink to={target} {...props} />;
 }
